0b39306c15c1b4905681b06edeeea7775dab1372
anastassiyatorovik <torovik1993@seznam.cz>
2023-01-07 00:43:52 +0100

    fix condition in while loop

--- reverse_file_buffered.py
+++ reverse_file_buffered.py
@@ -6,24 +6,24 @@ def reverse_file_in_memory(io: Io, name: str, buf_size: int):
     assert buf_size >= 2, "Minimum allowed buf_size is 2"
 
     length = io.file_length(name)
-    byte_size = buf_size//2
+    chunk_size = buf_size//2
     output_buff = bytearray(buf_size)
     left_offset = 0
-    right_offset = length - byte_size
+    right_offset = length - chunk_size
     remaining_to_reverse = length
 
-    while left_offset <= right_offset:
-        io.read_file(name, left_offset, byte_size, output_buff, 0)
-        io.read_file(name, right_offset, byte_size, output_buff, byte_size)
+    while left_offset < right_offset:
+        io.read_file(name, left_offset, chunk_size, output_buff, 0)
+        io.read_file(name, right_offset, chunk_size, output_buff, chunk_size)
 
         reversed_output_buff = output_buff[::-1]
 
-        io.write_file(reversed_output_buff, 0, byte_size, name, left_offset)
-        io.write_file(reversed_output_buff, byte_size, byte_size, name, right_offset)
+        io.write_file(reversed_output_buff, 0, chunk_size, name, left_offset)
+        io.write_file(reversed_output_buff, chunk_size, chunk_size, name, right_offset)
 
-        left_offset += byte_size
-        right_offset -= byte_size
-        remaining_to_reverse -= byte_size * 2
+        left_offset += chunk_size
+        right_offset -= chunk_size
+        remaining_to_reverse -= chunk_size * 2
     else:
         # reverse remaining characters
         if remaining_to_reverse > 1:
@@ -33,4 +33,4 @@ def reverse_file_in_memory(io: Io, name: str, buf_size: int):
 
 
 if __name__ == "__main__":
-    reverse_file_in_memory(FileIo(), name='test.txt', buf_size=3)
+    reverse_file_in_memory(FileIo(), name='test.txt', buf_size=4)
