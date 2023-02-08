bc6f266db8632cdb7f9504b4bbabbbcee4e5dfce
anastassiyatorovik <torovik1993@seznam.cz>
2023-02-08 22:14:15 +0100

    refactoring using mid index

--- reverse_file_buffered.py
+++ reverse_file_buffered.py
@@ -6,30 +6,24 @@ def reverse_file_in_memory(io: Io, name: str, buf_size: int):
     assert buf_size >= 2, "Minimum allowed buf_size is 2"
 
     length = io.file_length(name)
-    chunk_size = buf_size//2
-    output_buff = bytearray(chunk_size*2)
+    max_chunk = buf_size//2
+    output_buff = bytearray(max_chunk*2)
+    mid = length//2
     left_offset = 0
-    right_offset = length - chunk_size
-    remaining_to_reverse = length
+    chunk = min(max_chunk, mid - left_offset)
+    right_offset = length - (left_offset+chunk-1) - 1
 
-    while left_offset < right_offset and remaining_to_reverse > chunk_size*2:
-        io.read_file(name, left_offset, chunk_size, output_buff, 0)
-        io.read_file(name, right_offset, chunk_size, output_buff, chunk_size)
+    while left_offset < right_offset:
+        io.read_file(name, left_offset, chunk, output_buff, 0)
+        io.read_file(name, right_offset, chunk, output_buff, chunk)
 
         reversed_output_buff = output_buff[::-1]
 
-        io.write_file(reversed_output_buff, 0, chunk_size, name, left_offset)
-        io.write_file(reversed_output_buff, chunk_size, chunk_size, name, right_offset)
+        io.write_file(reversed_output_buff, 0, chunk, name, left_offset)
+        io.write_file(reversed_output_buff, chunk, chunk, name, right_offset)
 
-        left_offset += chunk_size
-        right_offset -= chunk_size
-        remaining_to_reverse -= chunk_size * 2
-
-    if remaining_to_reverse > 1:
-        output_buff = bytearray(remaining_to_reverse)
-        io.read_file(name, left_offset, remaining_to_reverse, output_buff, 0)
-        reversed_output_buff = output_buff[::-1]
-        io.write_file(reversed_output_buff, 0, remaining_to_reverse, name, left_offset)
+        left_offset += chunk
+        right_offset -= chunk
 
 
 if __name__ == "__main__":
