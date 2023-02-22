7d8be552b706224c122a649c979670a6568bb631
anastassiyatorovik <torovik1993@seznam.cz>
2023-02-22 09:12:26 +0100

    refactoring remove else

--- reverse_file_buffered.py
+++ reverse_file_buffered.py
@@ -21,18 +21,18 @@ def reverse_file_in_memory(io: Io, name: str, buf_size: int):
             reversed_output_buff = output_buff[::-1]
             io.write_file(reversed_output_buff, 0, chunk, name, left_offset)
             break
-        else:
-            io.read_file(name, left_offset, chunk, output_buff, 0)
-            io.read_file(name, right_offset, chunk, output_buff, chunk)
 
-            reversed_output_buff = output_buff[::-1]
+        io.read_file(name, left_offset, chunk, output_buff, 0)
+        io.read_file(name, right_offset, chunk, output_buff, chunk)
 
-            io.write_file(reversed_output_buff, 0, chunk, name, left_offset)
-            io.write_file(reversed_output_buff, chunk, chunk, name, right_offset)
+        reversed_output_buff = output_buff[::-1]
 
-            left_offset += chunk
-            right_offset -= chunk
+        io.write_file(reversed_output_buff, 0, chunk, name, left_offset)
+        io.write_file(reversed_output_buff, chunk, chunk, name, right_offset)
+
+        left_offset += chunk
+        right_offset -= chunk
 
 
 if __name__ == "__main__":
-    reverse_file_in_memory(FileIo(), name='test.txt', buf_size=20)
+    reverse_file_in_memory(FileIo(), name='test.txt', buf_size=6)
